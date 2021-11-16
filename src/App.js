964b71670457a38dc29023a8c50e74cbc1b8f5aa
aliM494 <88322172+aliM494@users.noreply.github.com>
2021-11-16 15:25:03 +0330

    separated elemnt and create tskContext

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { taskContext } from "./taskContext";
+import TaskItems from "./TaskItems";
+import TopForm from "./TopForm";
 
 const App = () => {
+  const [taskItems, settaskItems] = useState([
+    {
+      id: 1,
+      title: "کار شماره 1",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "کار شماره 2",
+      done: true,
+    },
+    {
+      id: 3,
+      title: "کار شماره 3",
+      done: false,
+    },
+  ]);
+
   return (
     <div className="container w-100 h-100 p-3">
       <div className="row h-100 justify-content-center align-align-items-start">
         <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
-          <h4 className="text-center text-info text_shdow">
-            به کدیاد خوش اومدید
-          </h4>
-          <form>
-            <div className="form-group d-flex">
-              <input type="text" className="form-control" />
-              <button type="submit" className="btn btn-success me-1">
-                ثبت
-              </button>
-            </div>
-          </form>
-          <ul className="list-group m-0 p-0 mt-2">
-            <li className="list-group-item d-flex justify-content-between">
-              کار شماره 1
-              <span>
-                <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-              </span>
-            </li>
-            <li className="list-group-item d-flex justify-content-between">
-              کار شماره 2
-              <span>
-                <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-              </span>
-            </li>
-          </ul>
+          <taskContext.Provider
+            value={{
+              taskItems,
+              settaskItems,
+            }}
+          >
+            <TopForm />
+            <TaskItems />
+          </taskContext.Provider>
         </div>
       </div>
     </div>
